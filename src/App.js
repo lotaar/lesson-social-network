33c4ed9bfb3a28b8d4bc5652177d0da80e8d84a2
lotaar <remnantnogosu@gmail.com>
2021-11-30 00:52:18 +0300

    active navlinks

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import "./App.css";
 import Profile from "./components/Profile/Profile";
 import Header from "./components/Header/Header";
-import Navbar from "./components/Navbar/Navbar";
+import { Navbar } from "./components/Navbar/Navbar";
 import Dialogs from "./components/Dialogs/Dialogs";
 
 import { BrowserRouter,Route, Routes } from "react-router-dom";
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,72 @@
+import classNames from "classnames";
 import React from "react";
-import styles from './Navbar.module.css'
+import { NavLink } from "react-router-dom";
+import styles from "./Navbar.module.css";
 
-const Navbar = () => {
-    return (
+export const Navbar = () => {
+  return (
     <nav className={styles.nav}>
-    <div className={styles.item}>
-        <a href='/profile'>Profile</a>
+      <div className={styles.item}>
+        <NavLink
+          className={({ isActive }) => classNames({ [styles.active]: isActive })}
+          to="/profile"
+        >
+          Profile
+        </NavLink>
       </div>
       <div className={styles.item}>
-        <a href='/dialogs'>Messages</a>
+        <NavLink
+          className={({ isActive }) => classNames({ [styles.active]: isActive })}
+          to="/dialogs"
+        >
+          Messages
+        </NavLink>
       </div>
       <div className={styles.item}>
-        <a href='/news' >News</a>
+        <NavLink
+          className={({ isActive }) => classNames({ [styles.active]: isActive })}
+          to="/news"
+        >
+          News
+        </NavLink>
       </div>
       <div className={styles.item}>
-        <a href='/music'>Music</a>
+        <NavLink
+          className={({ isActive }) => classNames({ [styles.active]: isActive })}
+          to="/music"
+        >
+          Music
+        </NavLink>
       </div>
       <div className={styles.item}>
-        <a href='settings'>Settings</a>
+        <NavLink
+          className={({ isActive }) => classNames({ [styles.active]: isActive })}
+          to="settings"
+        >
+          Settings
+        </NavLink>
       </div>
     </nav>
-    )
-}
+  );
+};
 
-export default Navbar;
+// // className={isActive =>
+// //   "nav-link" + (!isActive ? " unselected" : "")
+// // }
+
+// // className = { navData => navData.isActive ? s.active : s.item }
+
+// // className = {isActive =>
+// //   {s.item} +
+// // }
+
+// className = {styles.item}
+
+// activeClassName = {styles.active}
+
+// <NavLink
+//   to="/faq"
+//   className={}
+// >
+//   FAQs
+// </NavLink>
