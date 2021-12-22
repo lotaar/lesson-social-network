ef3e19194fb52a6f73e5c7ea3496da0750009efb
lotaar <remnantnogosu@gmail.com>
2021-12-22 23:01:05 +0300

    add preloader and refactor connect on users page

--- src/redux/users-reducer .js
+++ src/redux/users-reducer .js
@@ -3,46 +3,56 @@ const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
 const SET_CURRENT_PAGE='SET_CURRENT_PAGE';
 const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT'
+const TOGGLE_IS_FETCHING = 'TOGGLE_IS_FETCHING'
 
-export const followAC = (userId) => {
+export const follow = (userId) => {
   return {
     type: FOLLOW,
     userId,
   };
 };
 
-export const unfollowAC = (userId) => {
+export const unfollow = (userId) => {
   return {
     type: UNFOLLOW,
     userId,
   };
 };
 
-export const setUsersAC = (users) => {
+export const setUsers = (users) => {
   return {
     type: SET_USERS,
     users,
   };
 };
-export const setCurrentPageAC = (currentPage) => {
+export const setCurrentPage = (currentPage) => {
   return {
     type: SET_CURRENT_PAGE,
     currentPage,
   };
 };
 
-export const setTotalUsersCountAC = (count) => {
+export const setTotalUsersCount = (count) => {
   return {
     type: SET_TOTAL_USERS_COUNT,
     count,
   };
 };
+export const toggleIsFetching = (isFetching) => {
+  return {
+    type: TOGGLE_IS_FETCHING,
+    isFetching,
+  };
+};
+
+
 
 let initialState = {
   users: [],
   pageSize:5,
   totalUsersCount:0,
   currentPage:1,
+   isFetching:true,
 };
 
 const usersReducer = (state = initialState, action) => {
@@ -83,6 +93,11 @@ const usersReducer = (state = initialState, action) => {
         return{
           ...state,
           totalUsersCount:action.count
+        }
+        case TOGGLE_IS_FETCHING:
+        return{
+          ...state,
+          isFetching:action.isFetching
         }
  
     default:
