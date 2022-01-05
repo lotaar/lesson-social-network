4be18041234e372d0f36c889a38d022a4ade43a4
lotaar <remnantnogosu@gmail.com>
2022-01-05 22:23:39 +0300

    add ProfileStatus component with local state

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Preloader from "../../common/Preloader/Preloader";
 import styles from "./ProfileInfo.module.css";
+import ProfileStatus from './ProfileStatus.jsx'
 
 const ProfileInfo = (props) => {
    if(!props.profile) {
@@ -8,15 +9,15 @@ const ProfileInfo = (props) => {
    }
   return (
     <div>
-      <div>
+      {/* <div>
         <img
           alt=""
           src="https://images.ctfassets.net/hrltx12pl8hq/7yQR5uJhwEkRfjwMFJ7bUK/dc52a0913e8ff8b5c276177890eb0129/offset_comp_772626-opt.jpg?fit=fill&w=800&h=300"
         />
-      </div>
+      </div> */}
       <div className={styles.descriptionBlock}>
         <img src={props.profile.photos.large} alt="" />
-        ava+desc
+        <ProfileStatus status={'Как же хочется спать'}/>
         </div>
     </div>
   );
