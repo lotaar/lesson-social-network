9c6ac7735eb93caaf4fb88bd6cc72b961577e523
lotaar <remnantnogosu@gmail.com>
2022-01-19 22:09:46 +0300

    add reselect, hooks: useState

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Preloader from "../../common/Preloader/Preloader";
 import styles from "./ProfileInfo.module.css";
 import ProfileStatus from './ProfileStatus.jsx'
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = (props) => {
   
@@ -18,7 +19,7 @@ const ProfileInfo = (props) => {
       </div> */}
       <div className={styles.descriptionBlock}>
         <img src={props.profile.photos.large} alt="" />
-        <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+        <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
         </div>
     </div>
   );
---
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { useState } from "react";
+
+import styles from "./ProfileInfo.module.css";
+
+const ProfileStatusWithHooks = (props) => {
+  
+let [editMode, setEditMode] = useState(false)
+let [status, setStatus] = useState(props.status)
+
+// let stateWithSetState  = useState(false)
+
+// let editMode = stateWithSetState[0]
+// let setEditMode = stateWithSetState[1]
+
+let activateEditMode = () => {
+  setEditMode(true)
+}
+
+const deactivateEditMode =() => {
+  setEditMode(false)
+  props.updateStatus(status)
+}
+
+const onStatusChange = (e) => {
+  setStatus(e.currentTarget.value)
+    
+}
+
+   
+    return (
+      <div>
+        {!editMode && (
+          <div>
+            <span onDoubleClick={activateEditMode}>{props.status || "------"} </span>
+          </div>
+        )}
+        {editMode && (
+          <div>
+            <input onChange={onStatusChange} autoFocus={true}  value={status} onBlur={deactivateEditMode} />
+          </div>
+        )}
+      </div>
+    );
+  
+}
+
+export default ProfileStatusWithHooks;
